c27563f1e50bd55454070d65d33d48d0651555cd
John Lyons <johnlyonsdev@gmail.com>
2021-11-09 12:08:08 -0600

    Added multiple rounds, high scores, and guess limit

--- game.py
+++ game.py
@@ -2,51 +2,58 @@
 from random import randint
 
 print("Howdy, what's your name?")
-response = input()
+name = input()
 
-answer = randint(1,100)
+high = 101
 
-print(response + """, I'm thinking of a number between 1 and 100.
-Try to guess my number.""")
-
-count = 0
-while count >= 0:
-    guess = input()
-    print("Your guess? " + guess)
-    count += 1
-    if guess.isnumeric() == False:
-        print("That is not a valid number. Try again.")
-    else:
-        guess = int(guess)
-        if guess < 1 or guess > 100:
-            print("That is not between 1 and 100. Try again.") 
+def guessingGame(high):
+    answer = randint(1,100)
+    print(name + """, I'm thinking of a number between 1 and 100.\nTry to guess my number.\nYou will only have 10 guesses. Begin!""")
+    count = 0
+    while count >= 0:
+        guess = input()
+        print("Your guess? " + guess)
+        count += 1
+        if guess.isnumeric() == False:
+            print("That is not a valid number. Try again.")
         else:
-            if guess == answer:
-                print("Well done,", response, "You found my number in", count, "tries!")
-                break
-            elif guess > answer:
-                print("Your guess is too high, try again.")
+            guess = int(guess)
+            if guess < 1 or guess > 100:
+                print("That is not between 1 and 100. Try again.") 
             else:
-                print("Your guess is too low, try again.")
+                if guess == answer:
+                    print("Well done,", name, "You found my number in", count, "tries!")
+                    if count < high:
+                        high = count
+                        print('Congratulations on your new high score!')
+                    else:
+                        print("Oh no! You did not beat your high score of", high)
+                    print('To play again input "Yes". To quit, input something else.')
+                    response = input()
+                    if response == 'Yes':
+                        guessingGame(high)
+                    else:
+                        exit()
 
-                
-# alternate solution using try and except
+                elif guess > answer:
+                    if count > 9:
+                        print('Game over! You took too many tries!\n''To play again input "Yes". To quit, input something else.')
+                        response = input()
+                        if response == 'Yes':
+                            guessingGame(high)
+                        else:
+                            exit()
+                    else:
+                        print("Your guess is too high, try again.")
+                else:
+                    if count > 9:
+                        print('Game over! You took too many tries!\n''To play again input "Yes". To quit, input something else.')
+                        response = input()
+                        if response == 'Yes':
+                            guessingGame(high)
+                        else:
+                            exit()
+                    else:
+                        print("Your guess is too low, try again.")
 
-# while count >= 0:
-#     guess = input()
-#     print("Your guess? " + guess)
-#     count += 1
-#     try:
-#         guess = int(guess)
-#         if guess < 1 or guess > 100:
-#             print("That is not between 1 and 100. Try again.") 
-#         else:
-#             if guess == answer:
-#                 print("Well done,", response, "You found my number in", count, "tries!")
-#                 break
-#             elif guess > answer:
-#                 print("Your guess is too high, try again.")
-#             else:
-#                 print("Your guess is too low, try again.")
-#     except:
-#         print("This is not a valid number. Try again.")
+guessingGame(high)
